a9f04267164b84fdd48bedbf028cf0208c63a6ec
awiel <awiel@scapeler.com>
2019-05-12 21:29:26 +0200

    confg knmi serve

---
+++ openiod-fiware-connect-server-knmi-example.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+SYSTEMCODE="SCAPE604"
+SYSTEMPATH="/opt"
+
+LOGFILE=$SYSTEMPATH/$SYSTEMCODE/log/${SYSTEMCODE}-openiod-fiware-connect-server-knmi.log
+echo "Start procedure on: " `date` >>$LOGFILE
+
+cd $SYSTEMPATH/$SYSTEMCODE/openiod-fiware-connect
+# 'serve' and 'knmi' determine the name of the config file used for this session.
+/usr/bin/node openiod-fiware-connect serve knmi >>$LOGFILE
+
+echo "End   procedure on: " `date` >>$LOGFILE
+exit 0
